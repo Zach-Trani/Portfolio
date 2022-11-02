6cfabcecc289f58d68a8c5df49add0cfa3e59999
Zach-Trani <trani.zach@gmail.com>
2022-11-02 17:25:04 -0400

    added react template and App.css

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './App.css';
+
+const App = () => {
+    return <div>Hello World</div>
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'))
