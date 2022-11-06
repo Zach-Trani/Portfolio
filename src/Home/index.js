10bf5c3ed455318b5d8708ecf29cf6c1fb91c101
Zach-Trani <trani.zach@gmail.com>
2022-11-06 15:19:58 -0500

    color overhaul, matched button to color theme

--- src/Home/index.js
+++ src/Home/index.js
@@ -7,8 +7,8 @@ const Home = () => {
             <div className='home-container'>
                 <div className='home-left'>
                     <p className='home-text home-intro'>Hey, I'm Zach Trani</p>
-                    <h1 className='home-text home-header'>I enjoy designing modern websites for local companies</h1>
-                    <button className='home-text home-button'>Get in touch</button>
+                    <h1 className='home-text home-header'>I enjoy designing modern websites. For local business- by local business.</h1>
+                    <div className='home-text home-button'>Get in touch</div>
                 </div>
                 <div className='home-right'>
                     <div className='home-circle'>
--- src/NavBar/index.js
+++ src/NavBar/index.js
@@ -5,10 +5,9 @@ const NavBar = () => {
     console.log('hello')
     return (
         <div className='nav-container'>
-            <h1 className='nav-text'>Home</h1>
             <h1 className='nav-text'>About</h1>
-            <h1 className='nav-text'>Portfolio</h1>
-            <h1 className='nav-text'>Pages</h1>
+            <h1 className='nav-text'>Experience</h1>
+            <h1 className='nav-text'>Work</h1>
             <h1 className='nav-text'>Contact</h1>
         </div>
     );
